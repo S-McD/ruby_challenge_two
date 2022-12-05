3f7ba01fbf3a18b1c9bdcec87c3397cc452a9a8e
Simone <95197307+S-McD@users.noreply.github.com>
2022-12-05 19:12:35 +0000

    password manager challenge upload

--- program/lib/password_manager.rb
+++ program/lib/password_manager.rb
@@ -1,2 +1,19 @@
 class PasswordManager
+    def initialize
+        @new_hash = {}
+    end
+    
+    def add(service, password)
+        if (password.length >= 8) && (password =~ /[\W]/)
+        @new_hash[service] = password
+        end
+    end
+
+    def password_for(service)
+        return @new_hash.fetch(service)
+    end
+
+    def services
+        return @new_hash.keys
+    end
 end
